8018226b78fca6fc98a4714d83c686cd878e82b2
quyen.dang <quyen.dang@bys.vn>
2022-03-22 10:47:17 +0800

    - add animation logo

--- src/app/platforms/platforms.component.ts
+++ src/app/platforms/platforms.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./platforms.component.scss']
 })
 export class PlatformsComponent implements OnInit {
-
+  fakeMenu = [
+    {
+      title: 'Diagramming & Flowcharting',
+      desc: `Create powerful flowcharts and visuals to show clear processes that drive the better decision for your business.`
+    },
+    {
+      title: 'Floor Plan Design',
+      desc: `Offer a flexible and easy-to-use floor planning solution to maximize your space potential for everyone.`
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
