80c787b263edf46fef947184f2a99b290af34dff
KhanhHuyHo <huyho95@gmail.com>
2022-04-20 20:23:23 +0700

    - solutions

--- src/app/solutions/solutions.component.ts
+++ src/app/solutions/solutions.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+// @ts-ignore
+import AOS from 'aos';
 @Component({
   selector: 'app-solutions',
   templateUrl: './solutions.component.html',
-  styleUrls: ['./solutions.component.scss']
+  styleUrls: ['./solutions.component.scss'],
 })
 export class SolutionsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    AOS.init({
+      duration: 1500, // values from 0 to 3000, with step 50ms
+    });
   }
-
 }
